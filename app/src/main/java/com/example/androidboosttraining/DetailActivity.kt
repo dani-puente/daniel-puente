5dc809da0b27900f990f66332c326a0bab7b0c30
Daniel <PracticasSoftware1@proconsi.com>
2022-11-03 14:16:05 +0100

    Persistencia con el boton de favoritos
    El boton de fav se mantiene si le pulsas, queda almacenado en una  base de datos SQLite

--- app/src/main/java/com/example/androidboosttraining/DetailActivity.kt
+++ app/src/main/java/com/example/androidboosttraining/DetailActivity.kt
@@ -1,11 +1,13 @@
 package com.example.androidboosttraining
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
+import com.example.androidboosttraining.BD.DBHelper
 import com.example.androidboosttraining.databinding.ActivityDetailBinding
 import com.example.androidboosttraining.consulta_api_detalle.DetalleDBClient
 
@@ -13,28 +15,63 @@ import kotlinx.coroutines.launch
 
 
 class DetailActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        val extras = intent.extras
+        val idFicha = extras?.getInt("idFicha")
+        val dbHelper = DBHelper(this)
+        val db = dbHelper.writableDatabase
+        val estaEnFavoritos =
+            db.rawQuery("SELECT id, esFav FROM favorito where id = '$idFicha'", null)
+        if (estaEnFavoritos.moveToFirst()) {
+            if (estaEnFavoritos.getInt(1) == 0) {
+                binding.fab.setImageResource(R.drawable.ic_fav_vacio)
+                setContentView(binding.root)
+            } else {
+                binding.fab.setImageResource(R.drawable.ic_fav_lleno)
+                setContentView(binding.root)
+            }
+        } else {
+            binding.fab.setImageResource(R.drawable.ic_fav_vacio)
+            setContentView(binding.root)
+        }
         val galeriaAdapter = GaleriaAdapter(
             emptyList()
         )
+
+
         // A la hora de crear el recyclerView, es conveniente indicar el layaout y el adapter
         val manager = GridLayoutManager(this, 1)
         binding.recyclerGaleria.layoutManager = manager
         binding.recyclerGaleria.adapter = galeriaAdapter
 
-        val extras = intent.extras
-        val idFicha = extras?.getInt("idFicha")
         val tituloActBar = extras?.getString("tituloActBar")
         binding.fab.setOnClickListener {
-            if () {
-                binding.fab.setImageResource(R.drawable.ic_fav_vacio)
+            val db = dbHelper.writableDatabase
+            val consulta = db.rawQuery("SELECT id, esFav FROM favorito where id = '$idFicha'", null)
+            if (consulta.moveToFirst()) {
+                if (consulta.getInt(1) == 0) {
+                    binding.fab.setImageResource(R.drawable.ic_fav_lleno)
+                    val registro = ContentValues()
+                    registro.put("esFav", 1)
+                    db.update("favorito", registro, "id = $idFicha", null)
+                } else {
+                    binding.fab.setImageResource(R.drawable.ic_fav_vacio)
+                    val registro = ContentValues()
+                    registro.put("esFav", 0)
+                    db.update("favorito", registro, "id = $idFicha", null)
+                }
             } else {
                 binding.fab.setImageResource(R.drawable.ic_fav_lleno)
+                val registro = ContentValues()
+                registro.put("id", "$idFicha")
+                registro.put("esFav", 1)
+                db.insert("favorito", null, registro)
             }
+            db.close()
         }
         if (tituloActBar != null) {
             title = tituloActBar
@@ -63,5 +100,7 @@ class DetailActivity : AppCompatActivity() {
             Log.d("DetailActivity", "Estoy dando error")
         }
     }
+
+
 }
 
--- app/src/main/java/com/example/androidboosttraining/MainActivity.kt
+++ app/src/main/java/com/example/androidboosttraining/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.androidboosttraining
 
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.androidboosttraining.BD.DBHelper
 import com.example.androidboosttraining.databinding.ActivityMainBinding
 import com.example.androidboosttraining.consulta_api.Ficha
 import com.example.androidboosttraining.consulta_api.FichaDBClient
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,7 +29,12 @@ class MainActivity : AppCompatActivity() {
         val manager = GridLayoutManager(this, 1)
         binding.recycler.layoutManager = manager
         binding.recycler.adapter = fichasAdapter
-
+        var baseCreada = false
+        if (baseCreada == false){
+            val dBHelper = DBHelper(this)
+            val db : SQLiteDatabase = dBHelper.writableDatabase
+            db.close()
+        }
         lifecycleScope.launch {
             val idCategoriaPadre = getString(R.string.idCategoriaPadre)
             val idIdioma = getString(R.string.idIdioma)
