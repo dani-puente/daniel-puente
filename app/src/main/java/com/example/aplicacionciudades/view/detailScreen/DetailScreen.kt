bfd579520d827b68d2e31e249471be3c72a25bc6
Daniel <PracticasSoftware1@proconsi.com>
2022-11-23 16:59:18 +0100

    WIP: Pantalla detalle
    Me quedo desarrollando la ui detalle

---
+++ app/src/main/java/com/example/aplicacionciudades/view/detailScreen/DetailScreen.kt
@@ -0,0 +1,17 @@
+package com.example.aplicacionciudades.view.detailScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DetailScreen() {
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun Preview(){
+    DetailScreen()
+}
